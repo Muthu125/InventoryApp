9f2d2221012cf40d8e3a50c9d7d1b03862740409
muthuvenkatesh <muthuvenkatesh@inxsasia.com>
2017-08-24 13:16:37 +0530

    addedstocks

--- src/DashBoard.js
+++ src/DashBoard.js
@@ -1,16 +1,34 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import Dropdown from 'muicss/lib/react/dropdown';
+import DropdownItem from 'muicss/lib/react/dropdown-item';
+
+
 import {
     Toolbar, ToolbarRow,
-    ToolbarSection, ToolbarTitle, Content, Tab, Tabbar, FormField, Switch
+    ToolbarSection, ToolbarTitle, Content, Tab, Tabbar, FormField, Switch,
+    Textfield, Button
 } from 'react-mdc-web/lib';
 
+
 class DashBoard extends React.Component {
+
     constructor(props) {
         super(props)
 
         this.state = {
-            activeTab: '1'
+            activeTab: '1',
+            category:'',
+            quantity: '',
+            unitPrice: '',
+            totalCost: '',
+            comments: ''
         };
+        this.orderStocks = this.orderStocks.bind(this);
+    }
+
+    orderStocks(e){
+        console.log()
     }
 
     render() {
@@ -22,7 +40,7 @@ class DashBoard extends React.Component {
                             <ToolbarTitle>Inventory MSF</ToolbarTitle>
                         </ToolbarSection>
                         <FormField id="unlock">
-                            <Switch 
+                            <Switch
                                 checked={this.state.checked}
                                 onChange={({ target: { checked } }) => {
                                     this.setState({ checked })
@@ -50,9 +68,74 @@ class DashBoard extends React.Component {
                             Reports
                             </Tab>
                         <span className="mdc-tab-bar__indicator"></span>
+
                     </Tabbar>
+
+                    {this.state.activeTab == '1' &&
+                        <h2>
+                            You have tab 1 unread messages.
+                    </h2>
+                    }
+                    {this.state.activeTab == '2' &&
+
+                        <div>
+                            <Dropdown color="primary" label="Choose a Category">
+                                <DropdownItem link="#/link1">Stationaries</DropdownItem>
+                                <DropdownItem>Snacks</DropdownItem>
+                                <DropdownItem>Dispensaries</DropdownItem>
+                                <DropdownItem>Cleaning Materials</DropdownItem>
+                            </Dropdown>
+
+                            <Textfield
+                                floatingLabel="Your Quantity"
+                                helptext="For example, No. of biscuits needed"
+                                helptextPersistent
+                                value={this.state.quantity}
+                                onChange={({ target: { value: quantity } }) => {
+                                    this.setState({ quantity })
+                                }}
+                            />
+                            <Textfield
+                                floatingLabel="Your Unit Price"
+                                helptext="For example, Rs.10/- per unit"
+                                helptextPersistent
+                                value={this.state.unitPrice}
+                                onChange={({ target: { value: unitPrice } }) => {
+                                    this.setState({ unitPrice })
+                                }}
+                            />
+                            <Textfield
+                                floatingLabel="Your Total Price"
+                                helptext="For example, units (10) * price(Rs.25/-) (Auto Calculation Total-Rs.250/-) "
+                                helptextPersistent
+                                value={this.state.totalCost}
+                                onChange={({ target: { value: totalCost } }) => {
+                                    this.setState({ totalCost })
+                                }}
+                            />
+                            <Textfield
+                                floatingLabel="Comments Here...."
+                                multiline
+                                rows="8"
+                                cols="40"
+                                value={this.state.comments}
+                                onChange={({ target: { value: comments } }) => {
+                                    this.setState({ comments })
+                                }}
+                            />
+<br/>
+                            <Button raised accent onClick={this.orderStocks}>Add Stocks</Button>
+                        </div>
+
+                    }
+                    {this.state.activeTab == '3' &&
+                        <h2>
+                            Reports Page
+                    </h2>
+                    }
+
                 </Content>
-            </div>
+            </div >
         );
     }
 }
--- src/Login.js
+++ src/Login.js
@@ -1,37 +1,39 @@
 import React, { Component } from 'react';
+// import { BrowserRouter } from 'react-router-dom';
 // import logo from './logo.svg';
 //import './App.css';
 import {
   Button, Toolbar, ToolbarRow,
-  ToolbarSection, ToolbarTitle, Textfield, Content
+  ToolbarSection, ToolbarTitle, Textfield, Content, Tab, Tabbar, FormField, Switch
 } from 'react-mdc-web/lib';
 import 'material-components-web/dist/material-components-web.min.css';
-
+// import DashBoard from './DashBoard.js';
+// import Dropdown from 'muicss/lib/react/dropdown';
+// import DropdownItem from 'muicss/lib/react/dropdown-item';
 
 var Parse = require('parse');
 Parse.initialize("05b6e770a6189ba10731fd4686cd695a187b8612");
 Parse.serverURL = 'http://35.194.184.154:80/parse/'
 
-class Login extends Component {
-  // <div className="App">
-  //   <div className="App-header">
-  //     <img src={logo} className="App-logo" alt="logo" />
-  //     <h2>Welcome to React</h2>
-  //   </div>
-  //   <p className="App-intro">
-  //     To get started, edit <code>src/App.js</code> and save to reload.
-  //   </p>
-  // </div>
 
+class Login extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      username: '',
-      password: ''
+      username: 'muthu',
+      password: 'android',
+      loginStatus: false,
+      activeTab: '1',
+      category: '',
+      quantity: '',
+      unitPrice: '',
+      totalCost: '',
+      comments: ''
     };
     this.handleChange = this.handleChange.bind(this);
     this.submitLogin = this.submitLogin.bind(this);
+    this.orderStocks = this.orderStocks.bind(this);
   }
 
   handleChange({ target }) {
@@ -40,27 +42,167 @@ class Login extends Component {
     });
   }
 
+  tryConvert(value, convert) {
+    const rounded = value * convert;
+    return rounded.toString();
+  }
 
   submitLogin(e) {
     console.log("reached ........." + this.state.username);
     Parse.User.logIn(this.state.username, this.state.password, {
-      success: function (user) {
-        // Do stuff after successful login.  
-       // alert("Success chk: ");
-        window.open("http://localhost:3000/DashBoard", "_self");
-        // this.setState.loginStatus({
-        //   loginStatus : true
-        // });
-
-      },//.bind(this),
+      success: function (user) {  
+        // alert("Success chk: ");
+        //window.open('./DashBoard.js', "_self");
+        this.setState({
+          loginStatus: true
+        })
+      }.bind(this),
       error: function (user, error) {
-        // The login failed. Check error to see why.
         alert("Error in app: " + error);
       }
     });
   };
 
+  orderStocks(e) {
+    // console.log("value=======" + this.state.category);
+    var Stocks = Parse.Object.extend("stocks");
+    var stock = new Stocks();
+
+    stock.set("quantity", this.state.quantity);
+    stock.set("unitprice", this.state.unitPrice);    
+    stock.set("totalprice", this.state.totalCost);
+    stock.set("category", this.state.category);
+    stock.set("comments", this.state.comments);
+
+    stock.save(null, {
+      success: function(stock) {
+        alert('New Stocks has been added successfully.');
+      },
+      error: function(stock, error) {
+        alert('Failed to add new stcok, with error code: ' + error.message);
+      }
+    });
+  };
+
   render() {
+    if (this.state.loginStatus) {
+
+      const qtyValue = this.state.quantity;
+      const unitValue = this.state.unitPrice;
+      this.state.totalCost = this.tryConvert(unitValue, qtyValue);
+      //return <Redirect to='./DashBoard.js' />;
+      return (
+        <div className="content-login">
+          <Toolbar fixed>
+            <ToolbarRow>
+              <ToolbarSection align="start">
+                <ToolbarTitle>Inventory MSF</ToolbarTitle>
+              </ToolbarSection>
+              <FormField id="unlock">
+                <Switch
+                  checked={this.state.checked}
+                  onChange={({ target: { checked } }) => {
+                    this.setState({ checked })
+                  }}
+                />
+                <label>LogOut</label>
+              </FormField>
+            </ToolbarRow>
+          </Toolbar>
+          <Content className="login-content">
+            <Tabbar>
+              <Tab
+                active={this.state.activeTab === 1}
+                onClick={() => { this.setState({ activeTab: 1 }) }}>
+                Stocks
+                  </Tab>
+              <Tab
+                active={this.state.activeTab === 2}
+                onClick={() => { this.setState({ activeTab: 2 }) }}>
+                Add Stocks
+                  </Tab>
+              <Tab
+                active={this.state.activeTab === 3}
+                onClick={() => { this.setState({ activeTab: 3 }) }}>
+                Reports
+                  </Tab>
+              <span className="mdc-tab-bar__indicator"></span>
+
+            </Tabbar>
+
+            {this.state.activeTab == '1' &&
+              <h2>
+                You have tab 1 unread messages.
+          </h2>
+            }
+            {this.state.activeTab == '2' &&
+
+              <div>
+                <br />
+                <br />
+                <select
+                  name="category"
+                  color="primary"
+                  onChange={this.handleChange}
+                  value={this.state.category}>
+                  <option value="Choose a Category">Choose a Category</option>
+                  <option value="Stationaries">Stationaries</option>
+                  <option value="Snacks">Snacks</option>
+                  <option value="Dispensaries" >Dispensaries</option>
+                  <option value="Cleaning Materials">Cleaning Materials</option>
+                </select>
+                <Textfield
+                  name="quantity"
+                  floatingLabel="Your Quantity"
+                  helptext="For example, No. of biscuits needed"
+                  helptextPersistent
+                  value={this.state.quantity}
+                  onChange={this.handleChange}
+                />
+                <Textfield
+                  name="unitPrice"
+                  floatingLabel="Your Unit Price"
+                  helptext="For example, Rs.10/- per unit"
+                  helptextPersistent
+                  value={this.state.unitPrice}
+                  onChange={this.handleChange}
+                />
+                <Textfield
+                  name="totalValue"
+                  floatingLabel="Your Total Price"
+                  helptext="For example, units (10) * price(Rs.25/-) (Auto Calculation Total-Rs.250/-) "
+                  helptextPersistent
+                  value={this.state.totalCost}
+                  disabled
+                />
+                <Textfield
+                  floatingLabel="Comments Here...."
+                  multiline
+                  rows="8"
+                  cols="40"
+                  value={this.state.comments}
+                  onChange={({ target: { value: comments } }) => {
+                    this.setState({ comments })
+                  }}
+                />
+                <br />
+                <br />
+                <br />
+                <Button raised accent onClick={this.orderStocks}>Add Stocks</Button>
+              </div>
+
+            }
+            {this.state.activeTab == '3' &&
+              <h2>
+                Reports Page
+          </h2>
+            }
+
+          </Content>
+        </div >
+      );
+
+    }
     return (
       <div>
         <Toolbar fixed>
@@ -70,7 +212,7 @@ class Login extends Component {
             </ToolbarSection>
           </ToolbarRow>
         </Toolbar>
-        <Content>
+        <Content className="login-content">
           <Textfield
             name="username"
             floatingLabel="Your name or email"
@@ -78,6 +220,7 @@ class Login extends Component {
             autoFocus={true}
             value={this.state.username}
             onChange={this.handleChange}
+            style={{ width: '200px' }}
           />
           <Textfield
             name="password"
@@ -90,7 +233,7 @@ class Login extends Component {
           />
           <Button raised accent onClick={this.submitLogin}>Login</Button>
         </Content>
-      
+
       </div>
     );
   }
